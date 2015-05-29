3e9bc3e624f7ab0efc65b0c72d2117f31e0b2002
Kevin Hooke <kevin.hooke@gmail.com>
2015-05-28 23:12:03 -0700

    online doc updates

--- src/main/webapp/js/SpotVizApp.js
+++ src/main/webapp/js/SpotVizApp.js
@@ -1,36 +1,23 @@
-var spotVizApp = angular.module('SpotVizApp', 
-	[
-	 	"ngRoute",
-	 	"ngAnimate",
-	 	"ui.date",
-	 	"ngMap",
-	 	"ui.bootstrap",
-	 	"SpotVizControllers"	]);
+var spotVizApp = angular.module('SpotVizApp', [ "ngRoute", "ngAnimate",
+		"ui.date", "ngMap", "ui.bootstrap", "SpotVizControllers" ]);
 
-//"uiGmapgoogle-maps"
-
-spotVizApp.config(['$routeProvider',
-    function($routeProvider) {
-      $routeProvider.
-      when('/', {
-          templateUrl: 'home.html',
-          controller: 'HomeController'
-        }).
-        when('/mapviz', {
-          templateUrl: 'mapviz/mapviz.html',
-          controller: 'SpotVizController'
-        }).
-        when('/upload', {
-          templateUrl: 'howToUpload.html'
-        }).        
-        when('/faq', {
-            templateUrl: 'faq.html'
-          }).
-        when('/about', {
-          templateUrl: 'about.html',
-          controller: 'AboutController'
-        }).
-        otherwise({
-          redirectTo: '/'
-        });
-    }]);
+spotVizApp.config([ '$routeProvider', function($routeProvider) {
+	$routeProvider.when('/', {
+		templateUrl : 'home.html',
+		controller : 'HomeController'
+	}).when('/mapviz', {
+		templateUrl : 'mapviz/mapviz.html',
+		controller : 'SpotVizController'
+	}).when('/upload', {
+		templateUrl : 'howToUpload.html'
+	}).when('/faq', {
+		templateUrl : 'faq.html'
+	}).when('/bug', {
+		templateUrl : 'bug.html'
+	}).when('/about', {
+		templateUrl : 'about.html',
+		controller : 'AboutController'
+	}).otherwise({
+		redirectTo : '/'
+	});
+} ]);
