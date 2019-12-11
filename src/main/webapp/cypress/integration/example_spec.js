3a549a419069ccee9a52ba4639ae1a23ef0f7d6e
kevinhooke <kevin.hooke@gmail.com>
2019-12-10 23:12:16 -0800

    cleanup and static file move

---
+++ src/main/webapp/cypress/integration/example_spec.js
@@ -0,0 +1,11 @@
+describe('My First Test', function() {
+  
+	it('example test', function() {
+    expect(true).to.equal(true)
+  })
+  
+  it('example failing test', function() {
+    expect(true).to.equal(false)
+  })
+  
+})
